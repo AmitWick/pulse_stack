f407d973d8af4a24458b4b366576b70b6d9827b9
Kamit6337 <kamit6337@gmail.com>
2026-06-23 14:12:34 +0530

    add /health route

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,6 +4,12 @@ const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
     return { HomePage: true };
   });
+
+  fastify.get("/health", async (request, reply) => {
+    return reply.code(200).send({
+      message: "Health of server is good",
+    });
+  });
 };
 
 export default root;
